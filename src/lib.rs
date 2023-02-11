436fb0d67c2863f2ec2e3d5f10c04f55ba39cded
SymmetricChaos <ajfraebel@gmail.com>
2023-02-11 17:09:13 -0500

    putting stochastic aside for now
    had issues with checking consistency between writer and builder

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,4 @@ pub mod builder;
 pub mod cursor;
 pub mod segment;
 pub mod system;
-mod test;
 pub mod writer;
--- src/writer.rs
+++ src/writer.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use rand::{seq::SliceRandom, Rng};
+//use rand::{seq::SliceRandom, Rng};
 
 pub fn write_lsystem(axiom: &str, rules: &HashMap<char, &str>, depth: usize) -> String {
     let mut expression = String::from(axiom);
@@ -18,26 +18,26 @@ pub fn write_lsystem(axiom: &str, rules: &HashMap<char, &str>, depth: usize) -> 
     expression
 }
 
-pub fn write_lsystem_stochastic<R: Rng>(
-    axiom: &str,
-    rules: &HashMap<char, Vec<(&str, f32)>>,
-    depth: usize,
-    rng: &mut R,
-) -> String {
-    let mut expression = String::from(axiom);
-    for _ in 0..depth {
-        let mut new = String::new();
-        for c in expression.chars() {
-            if let Some(s) = rules.get(&c) {
-                match s.choose_weighted(rng, |item| item.1) {
-                    Ok(s) => new.push_str(s.0),
-                    Err(e) => panic!("{}", e.to_string()),
-                }
-            } else {
-                new.push(c)
-            }
-        }
-        expression = new;
-    }
-    expression
-}
+// pub fn write_lsystem_stochastic<R: Rng>(
+//     axiom: &str,
+//     rules: &HashMap<char, Vec<(&str, f32)>>,
+//     depth: usize,
+//     rng: &mut R,
+// ) -> String {
+//     let mut expression = String::from(axiom);
+//     for _ in 0..depth {
+//         let mut new = String::new();
+//         for c in expression.chars() {
+//             if let Some(s) = rules.get(&c) {
+//                 match s.choose_weighted(rng, |item| item.1) {
+//                     Ok(s) => new.push_str(s.0),
+//                     Err(e) => panic!("{}", e.to_string()),
+//                 }
+//             } else {
+//                 new.push(c)
+//             }
+//         }
+//         expression = new;
+//     }
+//     expression
+// }
